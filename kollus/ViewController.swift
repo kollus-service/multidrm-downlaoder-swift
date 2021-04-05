05ceeffec5545bbdd3e9e8cc599f861ac9457617
hyeond <blind3256@!>
2021-04-05 23:04:31 +0900

    Initial Commit

---
+++ kollus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kollus
+//
+//  Created by HyeonD on 2021/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
